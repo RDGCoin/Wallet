3354005d36dbecdda6fe7e25f787fd00d6d64a6b
RDGCoin <71654781+RDGCoin@users.noreply.github.com>
2020-10-31 11:33:07 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   network: 'homestead',
-  scryptDiff: 15,
+  scryptDiff: 14,
   AddressRDG: '0x47740A5ac376f6e95464A1FAfFB0D980944Cf4Bf',
   AddressRDGCoin: '0x3987dC5E72CD99e12cC98eddD65ed345473aFfD3',
   AddressSwap: '0x74D972e9974D9cEe07D156b572C001eB7296A5b2',
